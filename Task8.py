807c59504dc510b767f856c3b11051b016862f7b
Mayrambek-temir <m.temirov2015@gmail.com>
2019-11-17 10:12:05 +0300

    task8

---
+++ Task8.py
@@ -0,0 +1,9 @@
+﻿num1 = int(input('Enter first number: '))
+num2 = int(input('Enter second number: '))
+
+if num2 >= num1:
+    for num1 in range(num1,num2+1):
+        print(num1)
+else:
+    print('Num2 less then num1')
+
